c4056bb8eb2567e007f227bc0251cf071a8e1f27
Rich Stokes <rich.stokes@gmail.com>
2019-08-22 12:10:37 -0700

    Script to test availability of all ingress records

---
+++ test-all-ingress/test-all-ingress.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Foreach ingress resource in cluster, attempt to connect/curl to it. Report results.
+
+INGS=($(kubectl get ing --all-namespaces | tail -n +2 | awk '{print $3}' | cut -d',' -f1))  # Array of all ingress records in the cluster
+
+for i in "${INGS[@]}"
+do
+   : 
+   # do whatever on $i
+   echo "Checking $i.."
+   curl -sL -w "HTTP Response: %{http_code}\\n" https://$i  -o /dev/null  # Test endpoint with curl, assiming HTTPS here
+   echo ""
+done
